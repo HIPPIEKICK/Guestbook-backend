da77ea62ff5ff195089b443dee5c0461f026fd5a
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2020-03-17 14:46:08 +0100

    Set up routes to post and get messages

--- server.js
+++ server.js
@@ -1,21 +1,54 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import mongoose from "mongoose"
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/Guestbook"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Model for messages
+const Message = mongoose.model("Message", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+// Get messages
+app.get("/", async (req, res) => {
+  const messages = await Message.find().sort({ createdAt: -1 }).limit(20).exec()
+  if (messages.length > 0) {
+    res.json(messages)
+  } else {
+    res.status(404).json({ message: "No messages" })
+  }
+})
+
+// Post new message
+app.post("/", async (req, res) => {
+  const { message } = req.body
+  const newMessage = new Message({ message })
+  try {
+    const savedMessage = await newMessage.save()
+    res.status(201).json(savedMessage)
+  }
+  catch (err) {
+    res.status(400).json({ errorMessage: "Couldn't save message", error: err.errors })
+    console.log(err)
+  }
+
 })
 
 // Start the server
